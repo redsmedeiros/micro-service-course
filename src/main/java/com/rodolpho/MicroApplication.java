4b0f2172b34467a06ba51b7fc44c9d02dbe20cfa
Rodolpho <redsmedeiros@gmail.com>
2023-04-17 17:38:41 -0300

    entidade de post

--- src/main/java/com/rodolpho/MicroApplication.java
+++ src/main/java/com/rodolpho/MicroApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.rodolpho.model.Course;
-import com.rodolpho.repository.CourseRepository;
+
 
 @SpringBootApplication
 public class MicroApplication {
@@ -15,18 +14,5 @@ public class MicroApplication {
 		SpringApplication.run(MicroApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDatabase(CourseRepository courseRepository){
-		return args -> {
-			courseRepository.deleteAll();
-
-			Course c = new Course();
-
-			c.setName("Angular");
-			c.setCategory("front-end");
-
-			courseRepository.save(c);
-		};
-	}
 
 }
---
+++ src/main/java/com/rodolpho/entity/Post.java
@@ -0,0 +1,31 @@
+package com.rodolpho.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    private String description;
+
+    private String content;
+}
