ab676d268e1370c35d819a6eed178c3db373ed7a
Rodolpho <redsmedeiros@gmail.com>
2023-04-18 15:27:47 -0300

    criando tipagem de resposta

---
+++ src/main/java/com/rodolpho/payload/PostResponse.java
@@ -0,0 +1,17 @@
+package com.rodolpho.payload;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PostResponse {
+    private List<PostDto> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
