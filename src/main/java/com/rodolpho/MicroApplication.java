ea0dcd4e8399f2e730a0f0e209daa0c56a49061d
Rodolpho <redsmedeiros@gmail.com>
2023-04-20 15:03:07 -0300

    instalando o mapper

--- src/main/java/com/rodolpho/MicroApplication.java
+++ src/main/java/com/rodolpho/MicroApplication.java
@@ -1,14 +1,22 @@
 package com.rodolpho;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 
 
 @SpringBootApplication
 public class MicroApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroApplication.class, args);
