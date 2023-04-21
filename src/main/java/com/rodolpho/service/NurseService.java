ac9be4f008a40cef6ff8e90f8e556ea70d8bcfa7
Rodolpho <redsmedeiros@gmail.com>
2023-04-21 16:50:43 -0300

    criando a tranferencia dto

--- src/main/java/com/rodolpho/service/NurseService.java
+++ src/main/java/com/rodolpho/service/NurseService.java
@@ -1,5 +1,9 @@
 package com.rodolpho.service;
 
+import com.rodolpho.payload.NurseDto;
+
 public interface NurseService {
     
+    //recebe um DTO de req e envia um DTO de res
+    NurseDto createNurse(NurseDto nurseDto);
 }
---
+++ src/main/java/com/rodolpho/service/impl/NurseServiceImpl.java
@@ -0,0 +1,39 @@
+package com.rodolpho.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.rodolpho.entity.Nurse;
+import com.rodolpho.payload.NurseDto;
+import com.rodolpho.repository.NurseRepository;
+import com.rodolpho.service.NurseService;
+
+@Service
+public class NurseServiceImpl implements NurseService {
+
+    private NurseRepository nurseRepository;
+
+    public NurseServiceImpl(NurseRepository nurseRepository){
+
+        this.nurseRepository = nurseRepository;
+    }
+
+    @Override
+    public NurseDto createNurse(NurseDto nurseDto) {
+
+        Nurse nurse = new Nurse();
+
+        nurse.setName(nurseDto.getName());
+        nurse.setEmail(nurseDto.getEmail());
+        nurse.setDescription(nurseDto.getDescription());
+        nurse.setPhone(nurseDto.getPhone());
+        nurse.setGender(nurseDto.getGender());
+        nurse.setCity(nurseDto.getCity());
+        nurse.setAge(nurseDto.getAge());
+        nurse.setGraduation(nurseDto.getGraduation());
+        nurse.setExperienceYears(nurseDto.getExperienceYears());
+
+        
+        return null;
+    }
+    
+}
