534ae66080b446767cb6b192377ef3ef56223f7a
Rodolpho <redsmedeiros@gmail.com>
2023-04-21 14:44:50 -0300

    criando as anotações de pre autorização

--- src/main/java/com/rodolpho/config/SecurityConfig.java
+++ src/main/java/com/rodolpho/config/SecurityConfig.java
@@ -1,20 +1,48 @@
 package com.rodolpho.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder(); 
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http.csrf().disable()
-            .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
+            .authorizeHttpRequests((authorize) -> authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
             ).httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsServeice(){
+        
+        UserDetails rodolpho = User.builder().username("rodolpho").password(passwordEncoder().encode("123456")).roles("USER").build();
+
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("123456")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(rodolpho, admin);
+        
     }
     
 }
--- src/main/java/com/rodolpho/controller/PostController.java
+++ src/main/java/com/rodolpho/controller/PostController.java
@@ -2,6 +2,7 @@ package com.rodolpho.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ public class PostController {
     }
 
     //create a post
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
